3b3b7c31a83899cdc7a7f187300addfc31dd3bc1
BrazenFox <marokhonovdmitry1998@gmail.com>
2020-10-23 18:06:27 +0300

    final

--- src/DatePOJO.java
+++ src/DatePOJO.java
@@ -4,31 +4,9 @@ public class DatePOJO {
     private final int day;
 
     public DatePOJO(int year, int month, int day) {
-        //Все арифметические действия в конструкторе предназначены для того, чтобы пользователь мог вводить дату в любом формате и ему не выдавался exception
-        //Например: 2019.2.29 - 29 февраля в 2019 году не было, поэтому коструктор автоматически создаст дату 2019.3.1 как это делает GregorianCalendar
-        int realDay = 0;
-        int realYear = 0;
-        int realMonth = 0;
-        for (int i = 0; i < Math.abs(1970 - year); i++) {
-            realDay += (1970 + i) % 4 == 1 ? 366 : 365;
-        }
-        for (int i = 0; i < (month - 1); i++) {
-            realDay += Month.values()[i % 12].getDays() + ((year % 4 == 0 && i == 1) ? 1 : 0);
-        }
-        realDay += day - 1;
-
-        while (realDay > ((1970 + realYear) % 4 == 0 ? 366 : 365)) {
-            realDay -= (1970 - 1 + realYear) % 4 == 0 ? 366 : 365;
-            realYear++;
-        }
-
-        while (realDay >= ((1970 + realYear) % 4 == 0 && realMonth == 1 ? Month.values()[realMonth].getDays() + 1 : Month.values()[realMonth].getDays())) {
-            realDay -= Month.values()[realMonth].getDays() + ((1970 + realYear) % 4 == 0 && realMonth == 1 ? 1 : 0);
-            realMonth++;
-        }
-        this.day = realDay + 1;
-        this.month = realMonth + 1;
-        this.year = ((year >= 1970) ? 1970 + realYear : 1970 - realYear);
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     public int getYear() {
@@ -48,9 +26,7 @@ public class DatePOJO {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         DatePOJO obj1 = (DatePOJO) obj;
-        return year == obj1.year &&
-                month == obj1.month &&
-                day == obj1.day;
+        return this.year == obj1.year && this.month == obj1.month && this.day == obj1.day;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,10 @@ public class Main {
     public static void main(String[] args) {
         DateWorkerImpl date = new DateWorkerImpl();
         DatePOJO dateP = new DatePOJO(2020, 10, 6);
+        System.out.println(date.isLeapYear(1));
         System.out.println(date.getDayOfWeek(dateP));
         System.out.println(date.toString(dateP));
         System.out.println(date.countDays(dateP));
-        GregorianCalendar c = new GregorianCalendar(1903, Calendar.FEBRUARY,129);// перенос на 1 марта
-        System.out.println(c.getTime());
-        DatePOJO date1 = new DatePOJO(1903,2,129);
-        System.out.println(date1.toString());
+
     }
 }
